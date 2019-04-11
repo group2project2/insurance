f378755b73e104673efc15ff9c9c8cb5b9595a61
SunnyKate1998 <38138503+SunnyKate1998@users.noreply.github.com>
2019-04-12 02:27:02 +0800

    Add files via upload
    insurance_detail XML and activity_my_claim XML

--- Insurance/gen/com/example/insurance/R.java
+++ Insurance/gen/com/example/insurance/R.java
@@ -26,6 +26,9 @@ some parent container.
          */
         public static final int auto_play=0x7f010000;
     }
+    public static final class color {
+        public static final int black=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -34,9 +37,9 @@ some parent container.
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-        public static final int my_btn_height=0x7f040002;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int my_btn_height=0x7f050002;
     }
     public static final class drawable {
         public static final int dot_selected=0x7f020000;
@@ -47,85 +50,111 @@ some parent container.
         public static final int usericon=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f08001d;
-        public static final int btn_insurance=0x7f080009;
-        public static final int btn_my_claim=0x7f08000c;
-        public static final int btn_my_help=0x7f08000f;
-        public static final int btn_my_info=0x7f08000e;
-        public static final int btn_my_insurance=0x7f08000d;
-        public static final int btn_my_logout=0x7f080010;
-        public static final int et_insurance=0x7f080008;
-        public static final int gv_insurance=0x7f080003;
-        public static final int img_insurance_item=0x7f080011;
-        public static final int img_my=0x7f08000a;
-        public static final int login_btn1=0x7f080016;
-        public static final int login_btn2=0x7f080015;
-        public static final int login_et1=0x7f080014;
-        public static final int login_et2=0x7f080013;
-        public static final int main_btn1=0x7f080000;
-        public static final int main_btn2=0x7f080001;
-        public static final int register_btn=0x7f08001c;
-        public static final int register_et1=0x7f080017;
-        public static final int register_et2=0x7f080018;
-        public static final int register_et3=0x7f080019;
-        public static final int register_et4=0x7f08001a;
-        public static final int register_et6=0x7f08001b;
-        public static final int spinner_insurance=0x7f080006;
-        public static final int tv_insurance_add=0x7f080004;
-        public static final int tv_insurance_insornum=0x7f080007;
-        public static final int tv_insurance_insty=0x7f080005;
-        public static final int tv_insurance_item=0x7f080012;
-        public static final int tv_insurance_pur=0x7f080002;
-        public static final int tv_username=0x7f08000b;
+        public static final int action_settings=0x7f09002d;
+        public static final int btn_claim=0x7f09000b;
+        public static final int btn_insurance=0x7f090013;
+        public static final int btn_my_claim=0x7f090016;
+        public static final int btn_my_help=0x7f090019;
+        public static final int btn_my_info=0x7f090018;
+        public static final int btn_my_insurance=0x7f090017;
+        public static final int btn_my_logout=0x7f09001a;
+        public static final int et_insurance=0x7f090012;
+        public static final int et_insurance_detail_end=0x7f090020;
+        public static final int et_insurance_detail_number=0x7f090008;
+        public static final int et_insurance_detail_start=0x7f09000a;
+        public static final int et_insurance_detail_type=0x7f090006;
+        public static final int gv_insurance=0x7f09000d;
+        public static final int img_insurance_detail=0x7f09001b;
+        public static final int img_insurance_item=0x7f090021;
+        public static final int img_my=0x7f090014;
+        public static final int login_btn1=0x7f090026;
+        public static final int login_btn2=0x7f090025;
+        public static final int login_et1=0x7f090024;
+        public static final int login_et2=0x7f090023;
+        public static final int lv_claim=0x7f090003;
+        public static final int main_btn1=0x7f090000;
+        public static final int main_btn2=0x7f090001;
+        public static final int register_btn=0x7f09002c;
+        public static final int register_et1=0x7f090027;
+        public static final int register_et2=0x7f090028;
+        public static final int register_et3=0x7f090029;
+        public static final int register_et4=0x7f09002a;
+        public static final int register_et6=0x7f09002b;
+        public static final int spinner_insurance=0x7f090010;
+        public static final int tv_claim_add=0x7f090004;
+        public static final int tv_claim_client=0x7f090005;
+        public static final int tv_claim_insurance=0x7f090007;
+        public static final int tv_claim_reason=0x7f090009;
+        public static final int tv_claim_title=0x7f090002;
+        public static final int tv_insurance_add=0x7f09000e;
+        public static final int tv_insurance_detail_end=0x7f09001f;
+        public static final int tv_insurance_detail_number=0x7f09001d;
+        public static final int tv_insurance_detail_start=0x7f09001e;
+        public static final int tv_insurance_detail_type=0x7f09001c;
+        public static final int tv_insurance_insornum=0x7f090011;
+        public static final int tv_insurance_insty=0x7f09000f;
+        public static final int tv_insurance_item=0x7f090022;
+        public static final int tv_insurance_pur=0x7f09000c;
+        public static final int tv_username=0x7f090015;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_my_claim=0x7f030001;
         public static final int activity_my_insurance=0x7f030002;
         public static final int activity_my_personal=0x7f030003;
-        public static final int insurance_item=0x7f030004;
-        public static final int login=0x7f030005;
-        public static final int register=0x7f030006;
+        public static final int insurance_detail=0x7f030004;
+        public static final int insurance_item=0x7f030005;
+        public static final int login=0x7f030006;
+        public static final int register=0x7f030007;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int insurance_btn=0x7f050022;
-        public static final int insurance_tv_add=0x7f05001f;
-        public static final int insurance_tv_insornum=0x7f050021;
-        public static final int insurance_tv_insty=0x7f050020;
-        public static final int insurance_tv_pur=0x7f05001e;
-        public static final int login_btn1=0x7f050015;
-        public static final int login_btn2=0x7f050016;
-        public static final int login_et1=0x7f050013;
-        public static final int login_et2=0x7f050014;
-        public static final int main_btn1=0x7f05001c;
-        public static final int main_btn2=0x7f05001d;
-        public static final int main_tv2=0x7f050003;
-        public static final int main_tv3=0x7f050004;
-        public static final int main_tv4=0x7f050005;
-        public static final int main_tv5=0x7f050006;
-        public static final int main_tv6=0x7f050007;
-        public static final int main_tv7=0x7f050008;
-        public static final int my_btn1=0x7f050009;
-        public static final int my_btn2=0x7f05000a;
-        public static final int my_btn3=0x7f05000b;
-        public static final int my_btn_claim=0x7f050017;
-        public static final int my_btn_help=0x7f05001a;
-        public static final int my_btn_info=0x7f050019;
-        public static final int my_btn_insurance=0x7f050018;
-        public static final int my_btn_logout=0x7f05001b;
-        public static final int register_btn=0x7f050012;
-        public static final int register_et1=0x7f05000c;
-        public static final int register_et2=0x7f05000d;
-        public static final int register_et3=0x7f05000e;
-        public static final int register_et4=0x7f05000f;
-        public static final int register_et5=0x7f050010;
-        public static final int register_et6=0x7f050011;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int claim_btn=0x7f060029;
+        public static final int claim_tv_add=0x7f060028;
+        public static final int claim_tv_client=0x7f06002a;
+        public static final int claim_tv_reason=0x7f06002b;
+        public static final int claim_tv_title=0x7f060027;
+        public static final int hello_world=0x7f060001;
+        public static final int insurance_btn=0x7f060022;
+        public static final int insurance_detail_end=0x7f060026;
+        public static final int insurance_detail_number=0x7f060024;
+        public static final int insurance_detail_start=0x7f060025;
+        public static final int insurance_detail_type=0x7f060023;
+        public static final int insurance_tv_add=0x7f06001f;
+        public static final int insurance_tv_insornum=0x7f060021;
+        public static final int insurance_tv_insty=0x7f060020;
+        public static final int insurance_tv_pur=0x7f06001e;
+        public static final int login_btn1=0x7f060015;
+        public static final int login_btn2=0x7f060016;
+        public static final int login_et1=0x7f060013;
+        public static final int login_et2=0x7f060014;
+        public static final int main_btn1=0x7f06001c;
+        public static final int main_btn2=0x7f06001d;
+        public static final int main_tv2=0x7f060003;
+        public static final int main_tv3=0x7f060004;
+        public static final int main_tv4=0x7f060005;
+        public static final int main_tv5=0x7f060006;
+        public static final int main_tv6=0x7f060007;
+        public static final int main_tv7=0x7f060008;
+        public static final int my_btn1=0x7f060009;
+        public static final int my_btn2=0x7f06000a;
+        public static final int my_btn3=0x7f06000b;
+        public static final int my_btn_claim=0x7f060017;
+        public static final int my_btn_help=0x7f06001a;
+        public static final int my_btn_info=0x7f060019;
+        public static final int my_btn_insurance=0x7f060018;
+        public static final int my_btn_logout=0x7f06001b;
+        public static final int register_btn=0x7f060012;
+        public static final int register_et1=0x7f06000c;
+        public static final int register_et2=0x7f06000d;
+        public static final int register_et3=0x7f06000e;
+        public static final int register_et4=0x7f06000f;
+        public static final int register_et5=0x7f060010;
+        public static final int register_et6=0x7f060011;
     }
     public static final class style {
         /** 
@@ -149,11 +178,11 @@ some parent container.
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
     public static final class styleable {
         /** Attributes that can be used with a SlidingSwitcherView.
